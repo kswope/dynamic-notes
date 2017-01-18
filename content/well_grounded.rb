6e2db1486b2127168319670a0a747fdd0ed2cac1
kswope <github-kevdev@snkmail.com>
2017-01-18 04:59:12 -0500

    sync

--- content/well_grounded.rb
+++ content/well_grounded.rb
@@ -11,8 +11,6 @@
 =end
 
 
-# emit
-
 def m
   return 1,2,3
 end
@@ -25,8 +23,6 @@ end
 
 print m #=> [1, 2, 3] #=x syntax error, unexpected ',', expecting keyword_end
 
-# /emit
-
 
 =begin
 <p>__send__ is an safer alternative to send if you are paranoid about somebody
@@ -38,6 +34,55 @@ safer to use public send for 'tainted' send parameters.</p>
 Object.new.send(:local_variables) #=> [:_]
 Object.new.public_send(:local_variables) #=x NoMethodError: private method `local_variables' called
 
+
+=begin
+<p>Beware that setter methods will return the set value, not what the method returns, for consistency</p>
+=end
+
+class My
+  def x=(x)
+    @x = x
+    'boooo' # <!'boooo'>
+  end
+  def x
+    @x
+  end
+end
+
+o = My.new
+p o.x = 1 #=> <!1> 1, not 'boooo'
+
+
+
+=begin
+<p>The default superclass of a class is Object </p>
+=end
+
+class MyClass
+end
+
+# same as
+
+class MyClass < Object
+end
+
+
+=begin
+<p>You can make a class like this:</p>
+=end
+
+my_class = Class.new do
+
+  def hello
+    puts :hello
+  end
+
+end
+
+my_class.new.hello #=> hello
+
+
+page 119
 
 
 <br />
