020015930f01ca0236c54f60a6893ee21769a674
kswope <github-kevdev@snkmail.com>
2018-04-30 21:04:51 -0400

    sync

---
+++ content/javascript_with_promises_2nd_pass.js
@@ -0,0 +1,45 @@
+
+/*
+<!--
+// emit
+'use strict'
+
+function log(...args){
+  console.log(...args)
+}
+
+// /emit
+-->
+*/
+
+
+/*
+<div class='header'>
+ <span>
+   Promise constructor and resolver
+</span> 
+</div>
+<blockquote>
+  When Promise is used as a constructor it requires a callback known as a
+  resolver function. The resolver serves two purposes: it receives the resolve
+  and reject arguments, which are functions used to update the promise once
+  the outcome is known, and any error thrown from the resolver is implicitly
+  used to reject the promise.
+</blockquote>
+*/
+
+
+
+
+
+
+<br />
+  <br />
+  <br />
+  <br />
+  <br />
+
+
+
+
+
