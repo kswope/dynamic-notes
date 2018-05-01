b1f832f53d1d31d216c61e4d809195f4b4abae5a
kswope <github-kevdev@snkmail.com>
2018-05-01 18:43:22 -0400

    sync

--- content/javascript_with_promises_2nd_pass.js
+++ content/javascript_with_promises_2nd_pass.js
@@ -17,7 +17,7 @@ function log(...args){
 <div class='header'>
  <span>
    Promise constructor and resolver
-</span> 
+ </span> 
 </div>
 <blockquote>
   When Promise is used as a constructor it requires a callback known as a
@@ -28,16 +28,57 @@ function log(...args){
 </blockquote>
 */
 
+/*
+<div class='header'>
+ <span>
+ Promise convenience functions
+ </span> 
+</div>
+*/
+
+//// Equivalent ways to create a resolved promise.
+
+new Promise((resolve) => {
+  resolve('the long way')
+})
+
+Promise.resolve('the short way')
+
+
+/*
+<div class='header'>
+ <span>
+  As a general rule, any function that uses a promise should also return a
+  promise.
+ </span> 
+</div>
+<blockquote>
+When a promise is not propagated, the calling code cannot know when the
+promise is fulfilled and thus cannot effectively perform work after the fact.
+It’s easy to ignore this rule when writing a function whose caller does not
+care when the async work is finished, but don’t be fooled. It’s much easier
+to return a promise that initially goes unused than to retrofit promises into
+a series of functions later on.
+</blockquote>
+*/
 
 
 
 
 
+
+
+
+/*
 <br />
-  <br />
-  <br />
-  <br />
-  <br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+*/
 
 
 
--- run-with-emit.sh
+++ run-with-emit.sh
@@ -1,3 +1,3 @@
 
 nodemon -w content/speaking_javascript_2nd_pass.js --exec \
-  ./'content_combine.rb emit content/speaking_javascript_2nd_pass.js | node '
+  ./'content_combine.rb emit content/javascript_with_promises_2nd_pass.js | node '
